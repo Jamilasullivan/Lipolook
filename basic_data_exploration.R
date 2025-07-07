abded86b098c78b9ba332abce692dcc5a66a7d4d
Jamilasullivan <sullivan-al-kadhomiyj@cardiff.ac.uk>
2025-07-07 23:34:58 +0100

    Starting basic data organisation.

--- basic_data_exploration.R
+++ basic_data_exploration.R
@@ -1,3 +1,12 @@
+## setting variables ###########################################################
+
 data_path <- "data/test_lipids.csv"
 lipid_list <- "data/all_lipids.csv"
+
+## reading in data #############################################################
+
+data <- read.csv(data_path)
+lipid_list <- read.csv(lipid_list)
+
+
 
